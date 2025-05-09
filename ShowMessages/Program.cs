889fa773dc24df33f58fb7c14937675b3067721b
Catiamn <catia.nascimento2006@gmail.com>
2025-05-09 13:23:52 +0100

    Add comment

--- ShowMessages/Program.cs
+++ ShowMessages/Program.cs
@@ -5,7 +5,8 @@ namespace ShowMessages
     public class Program
     {
         private static void Main(string[] args)
-        {
+        { 
+            //this is a comment
             Console.WriteLine("Hello LP1!");
         }
     }
